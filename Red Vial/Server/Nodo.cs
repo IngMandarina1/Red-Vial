86c6c7b55327ec1cd0bca826937c2fa7de408625
Nelson Barahona <nelsonluisbarahona@gmail.com>
2025-04-28 22:24:22 -0600

    Agregar archivos de proyecto.

---
+++ Red Vial/Server/Nodo.cs
@@ -0,0 +1,35 @@
+﻿namespace Red_Vial.Server
+{
+	public class Nodo
+	{
+		public string Informacion { get; set; }
+		public Nodo?  Norte { get; set; }
+		public Nodo?  Este { get; set; }
+		public Nodo?  Oeste { get; set; }
+		public Nodo?  Sur { get; set; }
+
+		public Nodo()
+		{
+			Informacion = string.Empty;
+			Norte = null;
+			Este  = null;
+			Oeste = null;
+			Sur   = null;
+		}
+
+		public Nodo(string informacion)
+		{
+			Informacion = informacion;
+			Norte = null;
+			Este  = null;
+			Oeste = null;
+			Sur   = null;
+		}
+
+		public override string ToString()
+		{
+			return $"{Informacion}";
+		}
+
+	}
+}
